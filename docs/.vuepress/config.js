53b90ae7ac78a91b26b0650a1af966ac9e5af264
linjl <linjl@citycloud.com.cn>
2019-12-23 11:04:53 +0800

    test travis-ci

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  base: '/vuepressDoc',
+  title: '你好啊',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
